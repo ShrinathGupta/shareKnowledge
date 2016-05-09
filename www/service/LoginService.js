f59f4b1275205f9d1a909330a500b07acb0e8c80
ShrinathGupta <shrinath161@gmail.com>
2016-05-09 23:34:36 +0530

    Revert "Latest changes"
    This reverts commit 03d8a774658b989cfa3b09d4d6c56f9dbb4be138.

--- www/service/LoginService.js
+++
@@ -1,7 +0,0 @@
-﻿app.service('LoginService', function (appHTTPService) {
-    this.LoginUser = function (user) {
-        var params = {  password: user.password,username: user.username };
-        return appHTTPService.post("", {});
-    };
-});
-
